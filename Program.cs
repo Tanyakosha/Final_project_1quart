be3299a03c7dda181a6b7f000094f81f696f866e
Tatyana <tanya.dyatchkova@gmail.com>
2023-01-24 17:27:48 +0300

    добавила задачу

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2",":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+
+string[] array1 = new string[] {"123", "2", "hello", "world", "1234", "Kazan", ":-)"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+FillArray(array1, array2);
+PrintArray(array2);
+
+string[] FillArray(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    Array.Resize(ref array2, count);
+    return array2;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
